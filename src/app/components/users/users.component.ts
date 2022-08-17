6d176fe6101ee0f0356ae983492fa49a113281ff
Prashanth <sampprashanth@deloitte.com>
2022-08-17 12:52:07 +0530

    first angular-APP

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+  // template: `<h3>USERS COMPONETS.....</h3>`,
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: gray;
+  //     }
+  //   `,
+  // ],
+})
+export class UsersComponent {}
